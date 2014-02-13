a073ce252f63633a796a34e86d979354b902d661
czconnolly <c.z.connolly@dundee.ac.uk>
2014-02-13 14:34:09 +0000

    fixed some errors. still getting typeerror

--- models.py
+++ models.py
@@ -1,30 +1,31 @@
-import mysqldb
+import MySQLdb
 
 class dbhandler():
-	connection=none
-	dbname='mydbase'
-	dbuser='dbusername'
-	dbpassword='dbpassword'
+	connection=None
+	dbname='czconnolly'
+	dbuser='czconnolly'
+	dbpassword='ginger1993'
 
-def_init_(self):
-	if dbhandler.connection == none:
-		dbhandler.connection = mysqldb.connect(db=dbhamdler.dbname, \
+        def __init__(self):
+	        if dbhandler.connection == None:
+		        dbhandler.connection = MySQLdb.connect(db=dbhandler.dbname, \
 user=dbhandler.dbuser, passwd=dbhandler.dbpassword)
 
 	def cursor(self):
-	return dbhandler.connection.cursor()
+		return dbhandler.connection.cursor()
 
 class gene():
 	gene_symbol=''
 	gene_title=''
 	geneid=''
 	probelist=[]
+	exprlist={}
 
-	def_init_(self,geneid): #or gene symbol#
+	def __init__(self,geneid): 
 		self.gene_id=geneid
 		db=dbhandler()
 		cursor=db.cursor()
-		sql='select gene_title, gene_symbol, geneid, from gene where gene_id=$s'
+		sql='select gene_title, gene_symbol, geneid from gene where geneid=%s'
 		cursor.execute(sql,(geneid,))
 
 		#query db. get result and populate class fields. 
@@ -32,29 +33,15 @@ class gene():
 		result= cursor.fetchone()
 		self.gene_title = result[0]
 		self.gene_symbol = result[1]
+		
+		probesql='select probe_names from probe where geneid=%s'
+		cursor.execute(probesql, (self.gene_id,))
+		for result in cursor.fetchall():
+			self.probelist.append (result[0])
 
+		exprsql='select expression,sampleID, from gene_expression where probe_names=%s'
+		cursor.execute(exprsql, (probesql,(self.gene_id,)))
+		for result in cursor.fetchall():
+			self.exprlist[result[1]]=result[0]
 
-
-
-#now fetch probes... probesql='select probeid from probe where gene id=$s
-
-def get_probe(self,probe_names) :
-                self.gene_names = probeID
-                db=dbhandler()
-                cursor=db.cursor()
-                sql='select probe_names, from probe where gene_id=$s'
-                cursor.execute(sql,(probe_names,))
-
-
-
-#fill in blank!!!#
-
-for result in cursor.fetchall():
-self.probelist.append (result[0])
-
-def get_expression(self,experiment):
-                db=dbhandler()
-                cursor=db.cursor()
-                sql='select expression, from gene_expression where probe_names=$s'
-                cursor.execute(sql,(expression,))
 
