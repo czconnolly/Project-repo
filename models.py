cff61a34db5524b8cfca8869c107d8b641d272f4
czconnolly <c.z.connolly@dundee.ac.uk>
2014-02-14 16:24:53 +0000

    3 steps back, still not working....

--- models.py
+++ models.py
@@ -23,26 +23,27 @@ class gene():
 	exprlist={}
 
 	def __init__(self,geneid): 
-		self.gene_id=geneid
+		self.geneid=geneid
+		self.gene_title=gene_title
+		self.gene_symbol=gene_symbol
 		db=dbhandler()
 		cursor=db.cursor()
-		sql='select gene_title, gene_symbol from gene where geneid=%s'
+		sql='select gene_title,gene_symbol,geneid from gene where geneid=%s'
 			 #execute the sql command
-		self.gene_id=geneid
-		cursor.execute(sql,(gene_id,))
+		
+	try:
+		cursor.execute(sql,(geneid,))
 			#query db. get result and populate class fields. 
-		result=cursor.fetchone()
-		for answer in result:
-				self.gene_title=answer[0]
-				self.gene_symbol=answer[1]
-			#now print fetched result
-		return result
-
-
-		#print 'self.gene_title=%s,self.gene_symbol=%s'\
-		#			(gene_title,gene_symbol)
-		#except: 
-			#print 'Error fetching data' 
+		results=cursor.fetchone()
+		self.gene_title=result[0]
+		self.gene_symbol=result[1]
+		self.geneid=result[2]
+ 
+		for result in cursor.fetchone():
+			print 'self.geneid=%s,self.gene_title=%s,self.gene_symbol=%s'\
+					(geneid,gene_title,gene_symbol)
+	except: 
+		print 'Error fetching data' 
 
 
 
